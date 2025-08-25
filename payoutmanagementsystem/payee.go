10814a8b322dbe0d0b96dcc1ab7156bacc25f9b6
KirthiInfra <kirthivasan@infraspec.dev>
2025-08-25 14:51:59 +0530

    Add validation for account number

--- payoutmanagementsystem/payee.go
+++ payoutmanagementsystem/payee.go
@@ -1,18 +1,31 @@
 package payoutmanagementsystem
 
+import (
+	"errors"
+	"strconv"
+)
+
 type payee struct {
 	beneficiaryName string
 	beneficiaryCode string
-	accNo int
-	ifsc string
-	bankName string
-	email string
-	mobile int
-	payeeCategory string
+	accNo           int
+	ifsc            string
+	bankName        string
+	email           string
+	mobile          int
+	payeeCategory   string
 }
 
 func NewPayee(name string, code string, accNumber int, ifsc string, bankName string,
-	email string, mobile int,payeeCategory string) *payee {
-		return &payee{beneficiaryName: name, beneficiaryCode: code, accNo: accNumber, ifsc: ifsc, 
-		bankName: bankName, email: email, mobile: mobile, payeeCategory: payeeCategory}
+	email string, mobile int, payeeCategory string) (*payee, error) {
+	if numberOfDigits(accNumber) != 10 && numberOfDigits(accNumber) != 16 {
+		return nil, errors.New("payoutmanagementsystem: NewPayee: payee should be created with account number of length 10 or 16")
+	}
+	return &payee{beneficiaryName: name, beneficiaryCode: code, accNo: accNumber, ifsc: ifsc,
+		bankName: bankName, email: email, mobile: mobile, payeeCategory: payeeCategory}, nil
+}
+
+func numberOfDigits(number int) int {
+	numString := strconv.Itoa(number)
+	return len(numString)
 }
--- payoutmanagementsystem/payee_test.go
+++ payoutmanagementsystem/payee_test.go
@@ -1,13 +1,28 @@
 package payoutmanagementsystem
 
 import (
+	"errors"
 	"testing"
 )
 
-
 func TestNewPayee(t *testing.T) {
-	payee := NewPayee("abc","123",6780003045,"cbin05648","cbi", "abc@gmail.com", 91234567892, "Employee")
-	if payee == nil {
+	_, err := NewPayee("abc", "123", 6780003045, "cbin05648", "cbi", "abc@gmail.com", 91234567892, "Employee")
+	if err != nil {
 		t.Fatal("payee should be created")
+	}
+}
+
+func TestPayeeCannotBeCreatedWithInvalidAccountNumberOfLengthOtherThan10(t *testing.T) {
+	_, err := NewPayee("abc", "123", 678000, "cbin05648", "cbi", "abc@gmail.com", 91234567892, "Employee")
+	expectedErr := errors.New("payoutmanagementsystem: NewPayee: payee should be created with account number of length 10 or 16")
+	if err == expectedErr {
+		t.Fatal("payee should not be created with invalid account number")
+	}
+}
+
+func TestNewPayeeShouldBeCreatedWithAccountNumberOfLength16(t *testing.T) {
+	_, err := NewPayee("abc", "123", 6780002345765432, "cbin05648", "cbi", "abc@gmail.com", 91234567892, "Employee")
+	if err != nil {
+		t.Fatal("payee should be created with account number of length 16")
 	}
 }
