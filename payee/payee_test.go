16a7fcb71c801fddeb353844cdbe0e30fefbcc78
KirthiInfra <kirthivasan@infraspec.dev>
2025-08-26 21:59:52 +0530

    Breakdown the tests into valid and invalid tests

--- payee/payee_test.go
+++ payee/payee_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-var errTests = []struct {
+var invalidPayeeFieldsTests = []struct {
 	beneficiaryName string
 	beneficiaryCode string
 	accNo           int
@@ -15,20 +15,41 @@ var errTests = []struct {
 	payeeCategory   string
 	expectedErr     error
 }{
-	{"abc", "123", 6780003045, "cbin05648", "cbi", "abc@gmail.com", 9123456789, "Employee", nil},
 	{"abc", "123", 678000, "cbin05648", "cbi", "abc@gmail.com", 91234567892, "Employee", ErrInvalidAccountNumber},
-	{"abc", "123", 6780002345765432, "cbin05648", "cbi", "abc@gmail.com", 9123456789, "Employee", nil},
 	{"abc", "123", 6780002345765432, "cbin05648", "cbi", "abc@gmail.com", 912345678, "Employee", ErrInvalidMobileNumber},
 	{"abc", "123", 6780002345765432, "cbin05648", "cbi", "abc.com", 9123456782, "Employee", ErrInvalidEmail},
 	{"", "123", 6780002345765432, "cbin05648", "cbi", "abc@gmail.com", 9123456782, "Employee", ErrEmptyName},
 	{"abc", "", 6780002345765432, "cbin05648", "cbi", "abc@gmail.com", 9123456782, "Employee", ErrEmptyCode},
 }
 
-func TestValidatePayee(t *testing.T) {
-	for _, tt := range errTests {
+func TestPayeeCannotBeCreatedWithInvalidValues(t *testing.T) {
+	for _, tt := range invalidPayeeFieldsTests {
 		_, err := NewPayee(tt.beneficiaryName, tt.beneficiaryCode, tt.accNo, tt.ifsc, tt.bankName, tt.email, tt.mobile, tt.payeeCategory)
 		if err != tt.expectedErr {
 			t.Fatalf("Expected Error: %v but Actual Error: %v", tt.expectedErr, err)
+		}
+	}
+}
+
+var validPayeeFieldsTests = []struct {
+	beneficiaryName string
+	beneficiaryCode string
+	accNo           int
+	ifsc            string
+	bankName        string
+	email           string
+	mobile          int
+	payeeCategory   string
+}{
+	{"abc", "123", 6780003045, "cbin05648", "cbi", "abc@gmail.com", 9123456789, "Employee"},
+	{"abc", "123", 6780002345765432, "cbin05648", "cbi", "abc@gmail.com", 9123456789, "Employee"},
+}
+
+func TestPayeeCanBeCreatedWithValidValues(t *testing.T) {
+	for _, tt := range validPayeeFieldsTests {
+		_, err := NewPayee(tt.beneficiaryName, tt.beneficiaryCode, tt.accNo, tt.ifsc, tt.bankName, tt.email, tt.mobile, tt.payeeCategory)
+		if err != nil {
+			t.Fatalf("Payee should be created but got error: %v", err)
 		}
 	}
 }
